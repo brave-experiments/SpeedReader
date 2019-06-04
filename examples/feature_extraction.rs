dde21c5e37fd62377de6fb98591960da2fa0899b
gpestana <g6pestana@gmail.com>
2019-06-04 22:46:56 +0100

    feat: Adds feature extraction for words, url_depth and meta features

--- examples/feature_extraction.rs
+++ examples/feature_extraction.rs
@@ -7,14 +7,19 @@ use speedreader::feature_extractor::FeatureExtractor;
 use std::fs::File;
 
 fn main() {
+    let url = String::from("http://example.com/hello/world/hello?again");
+    let doc_path = "./examples/html/2CdyGKStt9jwu5u.html";
+    //let doc_path = "./examples/html/gp-ex.html";
+    //let doc_path = "./examples/html/gp-ex2.html";
+
     let mut data = String::new();
-    let mut f = File::open("./examples/html/2CdyGKStt9jwu5u.html").expect("err reading doc");
+    let mut f = File::open(doc_path).expect("err reading doc");
     f.read_to_string(&mut data).expect("err to string");
 
-    let mut extractor = FeatureExtractor::from_document(data.clone());
+    let mut extractor = FeatureExtractor::from_document(data, url);
     let result = extractor.extract();
 
-    println!("features map len: {}", result.len());
+    println!("features map len: {}\n", result.len());
     for (k, v) in result.iter() {
         println!("{}: {}", k, v);
     }
