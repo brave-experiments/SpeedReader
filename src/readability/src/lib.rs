fffe1cb1bfe1257dfb51fd6fbf224c127962995b
Andrius Aucinas <aaucinas@brave.com>
2019-06-07 17:43:35 +0100

    Pulls in kumabook/readability for mapping
    Includes the most basic wiring up between the components (no tests or proper error handling yet). Modifies readability to expose the right API
    
    Includes contents of readability fully in this repo because it requires improvements that are easier managed within instead of another repository intended to use in different environments anyway

---
+++ src/readability/src/lib.rs
@@ -0,0 +1,11 @@
+#[macro_use]
+extern crate html5ever;
+extern crate regex;
+extern crate url;
+#[macro_use]
+extern crate lazy_static;
+
+pub mod extractor;
+pub mod scorer;
+pub mod dom;
+pub mod error;
